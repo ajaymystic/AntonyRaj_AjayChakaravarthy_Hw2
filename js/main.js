1beed1f955862af382f72ae78ff5de531db21b28
Ajay Chakaravarthy Antony Raj <71629228+ajaymystic@users.noreply.github.com>
2026-03-16 22:31:23 -0400

    Updated JS
    In this commit, Added initialvalue parameter to constructor, defaults to 0.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 import { Counter } from './counter.js';
 
 const counter1=new Counter("#counter-container1");
-const counter2=new Counter("#counter-container2");
-const newCounter=new Counter("#counter-con");
+const counter2=new Counter("#counter-container2",10);
+const newCounter=new Counter("#counter-con",5);
