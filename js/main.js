fe7fe5818744d71c151f595ee141691ceec5bb70
Ajay Chakaravarthy Antony Raj <71629228+ajaymystic@users.noreply.github.com>
2026-03-17 17:39:45 -0400

    Updated Js
    In this commit, Added new class called Step counter.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 import { Counter } from './counter.js';
+import { StepCounter } from './StepCounter.js';
 
 const counter1=new Counter("#counter-container1");
 const counter2=new Counter("#counter-container2",10);
-const newCounter=new Counter("#counter-con",5);
+const newCounter = new StepCounter("#counter-con", 0, 5);
